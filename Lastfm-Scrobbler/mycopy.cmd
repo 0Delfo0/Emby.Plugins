a2e7024b4cece7a801435d255dd8db8a11ab04ab
davide.delfanti <davide.delfanti@cla-it.com>
2018-01-04 09:11:16 +0100

    discard

---
+++ Lastfm-Scrobbler/mycopy.cmd
@@ -0,0 +1,1 @@
+copy  C:\Users\davide.delfanti\Documents\myWS\Emby.Plugins\Lastfm-Scrobbler\obj\Debug\netstandard1.3\Lastfm.dll C:\Users\davide.delfanti\Downloads\embyserver-windows-x64-3.2.60.0\programdata\plugins\Lastfm.dll
